472ecedb5a2aae03d1f65b3e5be0425d14d7f9cb
timofeizima <timofeizima@mail.ru>
2024-02-16 00:12:11 +0300

    team секция

--- js/script.js
+++ js/script.js
@@ -18,3 +18,40 @@ function toggleMenu(){
 burger.addEventListener('click' , toggleMenu);
 
 
+
+//team 
+
+function accordionTeam() {
+  const workers = document.querySelectorAll(".team__item");
+  const teamAccord = document.querySelector(".team");
+
+  teamAccord.addEventListener("click", function (event) {
+    event.preventDefault(); 
+    const target = event.target; 
+   
+    if (target.classList.contains("team__title")) {
+      const worker = target.parentNode; 
+      const content = target.nextElementSibling; 
+      const contentHeight = content.firstElementChild.clientHeight;
+
+      for (const iterator of workers) {
+        if (iterator !== worker) {
+          iterator.classList.remove("accordeon__item-active");
+          iterator.lastElementChild.style.height = 0;
+        }
+      }
+
+      if (worker.classList.contains("accordeon__item-active")) {
+        worker.classList.remove("accordeon__item-active");
+        content.style.height = 0;
+      } else {
+        worker.classList.add("accordeon__item-active");
+        content.style.height = contentHeight + "px";
+      }
+    }
+  });
+}
+
+accordionTeam();
+
+
