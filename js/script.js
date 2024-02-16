42d685365ad392291503f9f916f7398202dc86df
timofeizima <timofeizima@mail.ru>
2024-02-16 22:20:53 +0300

    team

--- js/script.js
+++ js/script.js
@@ -32,20 +32,20 @@ function accordionTeam() {
     if (target.classList.contains("team__title")) {
       const worker = target.parentNode; 
       const content = target.nextElementSibling; 
-      const contentHeight = content.firstElementChild.clientHeight;
+      const contentHeight = content.firstElementChild.clientHeight; 
 
       for (const iterator of workers) {
         if (iterator !== worker) {
-          iterator.classList.remove("accordeon__item-active");
+          iterator.classList.remove("team__item-active");
           iterator.lastElementChild.style.height = 0;
         }
       }
 
-      if (worker.classList.contains("accordeon__item-active")) {
-        worker.classList.remove("accordeon__item-active");
+      if (worker.classList.contains("team__item-active")) {
+        worker.classList.remove("team__item-active");
         content.style.height = 0;
       } else {
-        worker.classList.add("accordeon__item-active");
+        worker.classList.add("team__item-active");
         content.style.height = contentHeight + "px";
       }
     }
@@ -53,5 +53,4 @@ function accordionTeam() {
 }
 
 accordionTeam();
-
 
