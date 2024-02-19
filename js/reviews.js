cf6f333765da5a57815bd22a0ba969d1b8a61dda
timofeizima <timofeizima@mail.ru>
2024-02-19 22:00:28 +0300

    модификатор изменён

--- js/reviews.js
+++ js/reviews.js
@@ -5,20 +5,21 @@ const reviewWrapp = document.querySelectorAll('.reviews__item');
 reviewsBar.addEventListener('click' , e=>{
    e.preventDefault();
 
-  
-  if(e.target.classList.contains('interactive-avatar__link')){
+  // console.log(Array.from(reviewsBar.children))
+
+  if(e.target.classList.contains('reviews__switcher-link')){
     const currentPeopleLink = e.target;
     const currentPeopleItem = currentPeopleLink.closest('.reviews__switcher-item')
     const currentPeopleId = currentPeopleItem.id;
     
     for (const iterator of reviewsItems) {
       if(iterator !== currentPeopleItem){
-        iterator.classList.remove("interactive-avatar--active");
+        iterator.classList.remove("reviews__switcher-item--active");
       }
     }
     
-    if(!currentPeopleItem.classList.contains("interactive-avatar--active")){
-      currentPeopleItem.classList.add("interactive-avatar--active");
+    if(!currentPeopleItem.classList.contains("reviews__switcher-item--active")){
+      currentPeopleItem.classList.add("reviews__switcher-item--active");
     }
     
     reviewWrapp.forEach(function(wrapp){
@@ -31,20 +32,34 @@ reviewsBar.addEventListener('click' , e=>{
   }
 });
 */
+
+
+
+
+
+
+
+
+
+
+
+
+
 const findBlockByAlias = (alias) => {
   return $(".reviews__item").filter((ndx, item) => {
     return $(item).attr("data-selected") === alias; 
   });
 };
 
-$(".interactive-avatar__link").click((e) => {
+$(".reviews__switcher-link").click((e) => {
   e.preventDefault();
 
   const $this = $(e.currentTarget);
   const target = $this.attr("data-select");
   const itemShow = findBlockByAlias(target);
-  const curItem = $this.closest(".interactive-avatar");
+  const curItem = $this.closest(".reviews__switcher-item");
 
   itemShow.addClass("reviews__item--active").siblings().removeClass("reviews__item--active");
-  curItem.addClass("interactive-avatar--active").siblings().removeClass("interactive-avatar--active");
+  curItem.addClass("reviews__switcher-item--active").siblings().removeClass("reviews__switcher-item--active");
 });
+
