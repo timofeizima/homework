e38a5cffbf14672cd7ac304ba71abd62dde2d6fe
timofeizima <timofeizima@mail.ru>
2024-02-22 11:30:46 +0300

    map

--- js/map.js
+++ js/map.js
@@ -2,9 +2,31 @@ let myMap;
  
 const init = () => {
  myMap = new ymaps.Map("map", {
-   center: [55.76, 37.64],
-   zoom: 7
+   center: [55.752004, 37.576133],
+   zoom: 16,
+   controls: []
  });
+
+const coords = [
+     [55.752004, 37.576133],
+   ],
+   myCollection = new ymaps.GeoObjectCollection({}, {
+     draggable: false,
+     iconLayout: 'default#image',
+     iconImageHref: './img/icons/geo.svg',
+     iconImageSize: [58, 73],
+     iconImageOffset: [-35, -52]
+   });
+ 
+ for (let i = 0; i < coords.length; i++) {
+   myCollection.add(new ymaps.Placemark(coords[i]));
+ }
+ 
+ myMap.geoObjects.add(myCollection);
+ 
+ myMap.behaviors.disable('scrollZoom');
 };
+
+
  
 ymaps.ready(init);
